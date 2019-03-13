dd0c223ca5384406bc47ccb68ab250ea065a8724
Jeff Adams <atomicxistence@gmail.com>
2019-03-13 16:28:03 -0400

    Remove Console and replace with View

--- SpaceTrucker.Console/Program.cs
+++ SpaceTrucker.Console/Program.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SpaceTrucker.Console
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-
+			new Game().Run();
 		}
 	}
 }
