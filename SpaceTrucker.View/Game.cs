224853294aa198bdee84505447147960dd678927
Jeff Adams <atomicxistence@gmail.com>
2019-03-13 20:47:56 -0400

    Add Game class

---
+++ SpaceTrucker.View/Game.cs
@@ -0,0 +1,31 @@
+﻿namespace SpaceTrucker.View
+{
+	class Game
+	{
+		public void Run()
+		{
+			Initialize();
+
+			while (true)
+			{
+				MainMenu();
+				GameLoop();
+			}
+		}
+
+		private void Initialize()
+		{
+			//TODO: intialize classes
+		}
+
+		private void MainMenu()
+		{
+			//TODO: main menu loop
+		}
+
+		private void GameLoop()
+		{
+			//TODO: game loop
+		}
+	}
+}
