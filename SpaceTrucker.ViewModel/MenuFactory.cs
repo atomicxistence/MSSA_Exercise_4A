79d4ef3cacc4bd5a9cdcc8be19be47bff890511d
Jeff Adams <atomicxistence@gmail.com>
2019-03-19 14:35:13 -0400

    Add methods to MenuFactory

--- SpaceTrucker.ViewModel/MenuFactory.cs
+++ SpaceTrucker.ViewModel/MenuFactory.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using SpaceTrucker.Models;
 
 namespace SpaceTrucker.ViewModel
 {
@@ -21,25 +20,50 @@ namespace SpaceTrucker.ViewModel
 
 		private void CreateMainMenu()
 		{
+			var options = new List<IOption>
+			{
+				new Option("Continue Game", false),
+				new Option("New Game", false),
+				new Option("Quit", false),
+			};
 
+			MainMenu = new Menu("Main Menu - Select an option below", options);
 		}
 
 		private void CreateGameMenu()
 		{
-			//travel
-			//market
-			//buy fuel
-			//main menu
+			var options = new List<IOption>
+			{
+				new Option("Travel to a different planet", false),
+				new Option("View the planet's trade market", false),
+				new Option("Purchase more fuel cells", false),
+				new Option("Back to Main Menu", false),
+			};
+
+			GameMenu = new Menu("Select an option below", options);
 		}
 
 		private void CreateTravelMenu()
 		{
+			var options = new List<IOption>();
 
+			foreach (var planet in Economy.planets)
+			{
+				options.Add(new Option($"{planet.ShortName} - {planet.Name}", false));
+			}
+
+			TravelMenu = new Menu("Select a planet to travel to...", options);
 		}
 
 		private void CreateConfirmationMenu()
 		{
+			var options = new List<IOption>
+			{
+				new Option("Yes", false),
+				new Option("No", false),
+			};
 
+			Confirmation = new Menu("Are you sure?", options);
 		}
 	}
 }
